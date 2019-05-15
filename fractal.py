02f421f2cfa60650bb83d0d45d3134deca5ee7cb
Jason Venturini <039799s@sd44.ca>
2019-05-15 12:43:38 -0700

    Created fractal.py

--- fractal.py
+++ fractal.py
@@ -2,35 +2,45 @@ from perlin_noise import Perlin
 import numpy
 from PIL import Image
 
-# def main():
-#     freq = 200  # Freq, smaller is more dense
-#     noise = Perlin(freq)
-#     noise.create_image(width=600, height=600, out_path='./noise.png')
+# https://www.scratchapixel.com/lessons/procedural-generation-virtual-worlds/procedural-patterns-noise-part-1/simple-pattern-examples
 
-# im1 = Perlin(25)
-# im1.create_image(width=600, height=600, out_path='./im1.png')
+# img1,img2,img3,img4,img5 = 0,0,0,0,0
+
+# imlist = [img1, img2, img3, img4, img5]
+
+# for idx,i in enumerate(imlist):
+#     i.create_image(width=600, height=600, out_path=f'{idx}.png')
+
+im0 = Perlin(2, seed='nu')
+im0.create_image(width=300, height=300, out_path='./im0.png')
+img0 = Image.open('./im0.png')
+
+im1 = Perlin(8, seed='nu')
+im1.create_image(width=300, height=300, out_path='./im1.png')
 img1 = Image.open('./im1.png')
 
-# im2 = Perlin(50)
-# im2.create_image(width=600, height=600, out_path='./im2.png')
+im2 = Perlin(16, seed='nu')
+im2.create_image(width=300, height=300, out_path='./im2.png')
 img2 = Image.open('./im2.png')
 
-# im3 = Perlin(75)
-# im3.create_image(width=600, height=600, out_path='./im3.png')
+im3 = Perlin(32, seed='nu')
+im3.create_image(width=300, height=300, out_path='./im3.png')
 img3 = Image.open('./im3.png')
 
-# im4 = Perlin(100)
-# im4.create_image(width=600, height=600, out_path='./im4.png')
+im4 = Perlin(64, seed='nu')
+im4.create_image(width=300, height=300, out_path='./im4.png')
 img4 = Image.open('./im4.png')
 
-# im5 = Perlin(125)
-# im5.create_image(width=600, height=600, out_path='./im5.png')
+im5 = Perlin(128, seed='nu')
+im5.create_image(width=300, height=300, out_path='./im5.png')
 img5 = Image.open('./im5.png')
+
+
 
 w,h = img1.size
 arr=numpy.zeros((h,w),numpy.float)
 
-imlist = [img1, img2, img3, img4, img5]
+imlist = [img0, img1, img2, img3, img4, img5]
 
 for im in imlist:
     imarr = numpy.array(im,dtype=numpy.float)
@@ -40,4 +50,4 @@ arr = numpy.array(numpy.round(arr),dtype=numpy.uint8)
 
 print(arr.shape)
 out=Image.fromarray(arr)
-out.save("Average.png")
+out.save("average.png")
