abb26ca409b1299a4b6a427dacfc40dc86f1a9c1
Ali El-Shazly <040710s@sd44.ca>
2019-05-17 11:55:04 -0700

    Removed frame from matplotlib

--- main.py
+++ main.py
@@ -5,26 +5,40 @@ import matplotlib.pyplot as plt
 from voronoi import *
 from perlin_noise import Perlin
 
-def main():
+# https://gist.github.com/kylemcdonald/bedcc053db0e7843ef95c531957cb90f
+def full_frame(width=None, height=None):
+    '''Removes frame from matplotlib'''
+    import matplotlib as mpl
+    mpl.rcParams['savefig.pad_inches'] = 0
+    figsize = None if width is None else (width, height)
+    fig = plt.figure(figsize=figsize)
+    ax = plt.axes([0,0,1,1], frameon=False)
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    plt.autoscale(tight=True)
 
+
+def main(size=30, freq=100, lloyds=2):
     # make up data points
-    size = 900
-    size_sqrt = int(size ** (1/2))
+    size_sqrt = size
+    size = size ** 2
+    # size_sqrt = int(size ** (1/2))
 
     # compute Voronoi tesselation
     vor = VoronoiDiagram(size)
     vor.generate_voronoi()
-    diagram = vor.relax_points(2)
+    diagram = vor.relax_points(lloyds)
 
     # Get noise, turn into 1D array
-    noise = Perlin(100)
-    noise_img = noise.create_image(save=True, width=500, height=500)
+    noise = Perlin(freq)
+    noise_img = noise.create_image(save=True, width=200, height=200)
     noise_gauss = noise.gaussian_kernel(noise_img)
-    noise_gauss.save('noise.png')
+    noise_gauss.save('images/noise.png')
     noise_resized = noise_gauss.resize((size_sqrt, size_sqrt))
     noise_arr = np.array(noise_resized)
 
-    plot(diagram, noise_arr)
+    plot(diagram, noise_arr, show_plot=False)
 
 if __name__ == '__main__':
+    full_frame()
     main()
