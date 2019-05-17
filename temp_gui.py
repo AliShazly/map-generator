0b5c21c07735c55f169aab48292d6f1564c59619
Ali El-Shazly <040710s@sd44.ca>
2019-05-17 11:55:59 -0700

    Added sliders to change map generator params

--- temp_gui.py
+++ temp_gui.py
@@ -1,15 +1,20 @@
 from tkinter import *
+import numpy as np
 from PIL import Image
+import matplotlib.pyplot as plt
 
+from voronoi import *
 from perlin_noise import Perlin
+import main as m
 
 class MainWindow():
 
     def __init__(self, main):
+
         main.title('Procedural Map Generator')
         main.resizable(False, False)
 
-        self.image_path = 'noise.png'
+        self.image_path = 'images/voro.png'
         self.image = PhotoImage(file=self.image_path)
         im = Image.open(self.image_path)
         self.width, self.height = im.size
@@ -22,19 +27,42 @@ class MainWindow():
         self.button = Button(main, text='Generate', command=self.on_button)
         self.button.grid(row=1, column=0)
 
+        self.button = Button(main, text='Reset Params', command=self.reset)
+        self.button.grid(row=2, column=0)
+
         self.slider_01_label = Label(main, text='Frequency: ')
-        self.slider_01_label.grid(row=2, column=0, sticky=W)
-        self.slider_01 = Scale(main, from_=0, to=150, orient=HORIZONTAL, length=self.width - 150)
-        self.slider_01.grid(row=2, column=0)
+        self.slider_01_label.grid(row=3, column=0, sticky=W)
+        self.slider_01 = Scale(main, from_=5, to=150, orient=HORIZONTAL, length=self.width - 150)
+        self.slider_01.set(30)
+        self.slider_01.grid(row=3, column=0)
+
+        self.slider_02_label = Label(main, text='Size: ')
+        self.slider_02_label.grid(row=4, column=0, sticky=W)
+        self.slider_02 = Scale(main, from_=2, to=100, orient=HORIZONTAL, length=self.width - 150)
+        self.slider_02.set(30)
+        self.slider_02.grid(row=4, column=0)
+
+        self.slider_03_label = Label(main, text='Smoothing: ')
+        self.slider_03_label.grid(row=5, column=0, sticky=W)
+        self.slider_03 = Scale(main, from_=0, to=5, orient=HORIZONTAL, length=self.width - 150)
+        self.slider_03.set(2)
+        self.slider_03.grid(row=5, column=0)
         
+    def reset(self):
+        self.slider_01.set(100)
+        self.slider_02.set(30)
+        self.slider_03.set(2)
 
     def on_button(self):
         frequency = self.slider_01.get()
-        frequency = 150 - frequency  # Reversing frequency to make more sense
-        noise = Perlin(frequency)
-        img = noise.create_image(width=600, height=600)
-        img_gauss = noise.gaussian_kernel(img)
-        img_gauss.save('noise.png')
+        # frequency = 150 - frequency  # Reversing frequency to make more sense
+
+        size = self.slider_02.get()
+
+        smoothing = self.slider_03.get()
+
+        m.full_frame()  # Removes frame from voronoi matplotlib
+        m.main(freq=frequency, lloyds=smoothing, size=size)
 
         self.image = PhotoImage(file=self.image_path)
         self.canvas.itemconfig(self.image_on_canvas, image=self.image)
@@ -44,3 +72,4 @@ root = Tk()
 
 MainWindow(root)
 root.mainloop()
+# root.destroy()
