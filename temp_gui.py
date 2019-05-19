d7ff36ef82acfafee114c9cf8cc37cacf3a4187d
AliShazly <alishazly302@gmail.com>
2019-05-19 21:49:42 +0000

    Changed defaults and added sigma

--- temp_gui.py
+++ temp_gui.py
@@ -33,7 +33,7 @@ class MainWindow():
         self.slider_01_label = Label(main, text='Frequency: ')
         self.slider_01_label.grid(row=3, column=0, sticky=W)
         self.slider_01 = Scale(main, from_=5, to=150, orient=HORIZONTAL, length=self.width - 150)
-        self.slider_01.set(30)
+        self.slider_01.set(40)
         self.slider_01.grid(row=3, column=0)
 
         self.slider_02_label = Label(main, text='Size: ')
@@ -47,11 +47,18 @@ class MainWindow():
         self.slider_03 = Scale(main, from_=0, to=5, orient=HORIZONTAL, length=self.width - 150)
         self.slider_03.set(2)
         self.slider_03.grid(row=5, column=0)
+
+        self.slider_04_label = Label(main, text='Sigma: ')
+        self.slider_04_label.grid(row=6, column=0, sticky=W)
+        self.slider_04 = Scale(main, from_=1, to=7, resolution=0.1, orient=HORIZONTAL, length=self.width - 150)
+        self.slider_04.set(3.0)
+        self.slider_04.grid(row=6, column=0)
         
     def reset(self):
-        self.slider_01.set(100)
+        self.slider_01.set(40)
         self.slider_02.set(30)
         self.slider_03.set(2)
+        self.slider_04.set(3.0)
 
     def on_button(self):
         frequency = self.slider_01.get()
@@ -61,8 +68,10 @@ class MainWindow():
 
         smoothing = self.slider_03.get()
 
+        sigma = self.slider_04.get()
+
         m.full_frame()  # Removes frame from voronoi matplotlib
-        m.main(freq=frequency, lloyds=smoothing, size=size)
+        m.main(freq=frequency, lloyds=smoothing, size=size, sigma=sigma)
 
         self.image = PhotoImage(file=self.image_path)
         self.canvas.itemconfig(self.image_on_canvas, image=self.image)
@@ -72,4 +81,4 @@ root = Tk()
 
 MainWindow(root)
 root.mainloop()
-# root.destroy()
+raise SystemExit
