43de348736270585d6168f7b7807bed0eb627f2b
AliShazly <alishazly302@gmail.com>
2019-05-21 00:29:09 +0000

    Added shallow water and optimized get_neighbors

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ import matplotlib.pyplot as plt
 
 from voronoi import *
 from perlin_noise import Perlin
-# https://gist.github.com/kylemcdonald/bedcc053db0e7843ef95c531957cb90f
+
 def full_frame(width=None, height=None):
+    # https://gist.github.com/kylemcdonald/bedcc053db0e7843ef95c531957cb90f
     '''Removes frame from matplotlib'''
     import matplotlib as mpl
     mpl.rcParams['savefig.pad_inches'] = 0
@@ -27,17 +28,30 @@ def timer(function):
         result = function(*args, **kwargs)
         end = time.time()
         elapsed = round(end - start, 2)
-        if elapsed > 0.0:
-            print(f'{function.__name__} took {elapsed} seconds.')
+        print(f'{function.__name__} took {elapsed} seconds.')
         return result
     return wrapper
 
 
 class MapGenerator:
+    
+    def _fill_polys(self, poly_list, color='r', alpha=1, single=False):
+        plt.xlim(.05, .95)
+        plt.ylim(.05, .95)
+        if single:
+            plt.fill(*zip(*poly_list), color, alpha=alpha)
+        else:
+            for i in poly_list:
+                plt.fill(*zip(*i), color, alpha=alpha)
 
-    def _fill_polys(self, poly_list, color='r', alpha=1):
-        for i in poly_list:
-            plt.fill(*zip(*i), color, alpha=alpha)
+    def _distance(self, pt1, pt2):
+        return np.sqrt((pt2[0] - pt1[0])** 2 + (pt2[1] - pt1[1])** 2)
+
+    def _polygon_is_in(self, poly, poly_lst):
+        for i in poly_lst:
+            if i == poly:
+                return True
+        return False
 
     def _get_centroid(self, polygon):
         x_vals = [i[0] for i in polygon]
@@ -49,9 +63,6 @@ class MapGenerator:
     def _get_polygon(self, centroid):
         index = self.centroids.index(centroid)
         return self.polygons[index]
-
-    def _distance(self, pt1, pt2):
-        return np.sqrt((pt2[0] - pt1[0])** 2 + (pt2[1] - pt1[1])** 2)
     
     def _get_neighbors(self, main_polygon):
         x, y = main_polygon[0][0], main_polygon[0][1]
@@ -66,23 +77,34 @@ class MapGenerator:
             for polygon in polygons_close:
                 if point in polygon:
                     polygon_neighbors.append(polygon)
-        return polygon_neighbors
+
+        # Removing duplicates
+        neighbors = list(set([tuple([tuple(point) for point in polygon])
+            for polygon in polygon_neighbors]))
+        
+        # Turning back into a nested list
+        neighbors = [[list(point) for point in polygon] for polygon in neighbors]
+
+        # Removing main polygon
+        if self._polygon_is_in(main_polygon, neighbors):
+            neighbors.remove(main_polygon)
+        
+        return neighbors
 
     @timer
     def _generate_base_terrain(self, noise_arr):
         self.land_polygons = []
         self.water_polygons = []
-
         for polygon in self.polygons:
-
             # idk why i need to do this. self.polygons changes otherwise and i have no clue why
-            p = polygon.copy(order='C')
-            po = polygon.copy(order='C')
+            p = polygon.copy()
+            po = polygon.copy()
 
-            polygon_stripped = [i for i in po if 0 <= i[0] <= 1 and 0 <= i[1] <= 1]
+            polygon_stripped = [i for i in po if 0 <= i[0] <= 1
+                                    and 0 <= i[1] <= 1]
 
-            coords = polygon_stripped[0]
-            coords *= noise_arr.shape[0]
+            coords = self._get_centroid(polygon_stripped)
+            coords = [i * noise_arr.shape[0] for i in coords]
 
             x = int(round(coords[0]))
             y = int(round(coords[1]))
@@ -99,48 +121,64 @@ class MapGenerator:
 
     @timer
     def add_beaches(self):
-        # TODO: Do this differently. This is extremely dumb
-        water_sums = [np.sum(i) for i in self.water_polygons]
-
         self.beach_polys = []
         beach_indicies = []
         for idx, land_poly in enumerate(self.land_polygons):
             flag = False
             neighbors = self._get_neighbors(land_poly)
             for i in neighbors:
-                if np.sum(i) in water_sums:
+                if self._polygon_is_in(i, self.water_polygons):
                     flag = True
             if flag:
                 self.beach_polys.append(land_poly)
                 beach_indicies.append(idx)
-
-        # Overwriting land cells
-        for i in sorted(beach_indicies, reverse=True):
-            del self.land_polygons[i]
-
-    def generate_temperature_points(self, min_distance=0.1):
-        hot_polygon, cold_polygon = random.sample(self.land_polygons, 2)
         
-        hot_point = self._get_centroid(hot_polygon)
-        cold_point = self._get_centroid(cold_polygon)
+        # Overwriting land cells
+        for idx in sorted(beach_indicies, reverse=True):
+            del self.land_polygons[idx]
 
-        # TODO: Do this differently, recursion breaks too easily
-        if self._distance(hot_point, cold_point) < min_distance:
-            return self._generate_temperature_points(self.land_polygons)
-    
     @timer
-    def generate_map(self, size=50, freq=20, lloyds=3, sigma=3.15):
+    def add_deep_water(self):
+        water_sorted = sorted(self.water_polygons, key=lambda x: x[0][0])
+        self.deep_water_polys = []
+
+        def check_validity(poly):
+            if not self._polygon_is_in(poly, self.water_polygons):
+                return False
+            if self._polygon_is_in(poly, self.deep_water_polys):
+                return False
+            return True
+
+        queue = [water_sorted[10]]
+        while queue:
+            poly = queue.pop()
+
+            # Sometimes it iterates on duplicates, this fixes it.
+            if not check_validity(poly):
+                continue
+            
+            self.water_polygons.remove(poly)
+            self.deep_water_polys.append(poly)
+            neighbors = self._get_neighbors(poly)
+                
+            for i in neighbors:
+                if check_validity(i):
+                    queue.append(i)
+
+    @timer
+    def generate_map(self, size=75, freq=20, lloyds=2, sigma=3.15):
         # make up data points
         size_sqrt = size
         size = size ** 2
 
         # compute Voronoi tesselation
-        vor = VoronoiDiagram(size)
+        points = np.random.random((size, 2))
+        vor = VoronoiDiagram(points)
         vor.generate_voronoi()
         self.voronoi_diagram = vor.relax_points(lloyds)
         regions, vertices = voronoi_finite_polygons_2d(self.voronoi_diagram)
-
-        self.polygons = [vertices[region] for region in regions]
+        
+        self.polygons = clip(points, regions, vertices)
         self.centroids = [self._get_centroid(i) for i in self.polygons]
 
         # Get noise, turn into 1D array
@@ -156,18 +194,16 @@ class MapGenerator:
     @timer
     def plot(self):
         land = '#37754D'
-        water = '#285A8F'
+        water_deep = '#285A8F'
+        water_shallow = '#2685A6'
         beach = '#877965'
+        self._fill_polys(self.polygons, 'black')
         self._fill_polys(self.land_polygons, land)
-        self._fill_polys(self.water_polygons, water)
+        self._fill_polys(self.water_polygons, water_shallow)
         self._fill_polys(self.beach_polys, beach)
-
-        plt.xlim(0, 1)
-        plt.ylim(0, 1)
+        self._fill_polys(self.deep_water_polys, water_deep)
 
         plt.savefig('images/voro.png', dpi=150)
-        # plt.show()
-
 
 
 if __name__ == '__main__':
@@ -175,5 +211,6 @@ if __name__ == '__main__':
     generator = MapGenerator()
     generator.generate_map()
     generator.add_beaches()
+    generator.add_deep_water()
 
     generator.plot()
